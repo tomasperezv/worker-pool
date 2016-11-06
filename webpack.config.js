5f54886baadb6c5c707f70396bd1c81edd6098c6
Tomás Pérez <t@0x101.com>
2016-11-06 20:13:40 +0100

    Adding html-webpack for generating the example html.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   context: __dirname,
   entry: {
-    'worker-pool': [
+    'worker-pool.js': [
       './src/pool.js',
       './src/worker-pool.js'
     ]
   },
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: '[name].js'
-  }
+    filename: '[name]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './example/worker-pool-example.html',
+      filename: 'example.html'
+    })
+  ]
 };
 
 module.exports = config;
